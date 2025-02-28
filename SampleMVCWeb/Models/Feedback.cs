221f02f76c3294604cb619af431ab12ff3eebe91
Rishi Prajapati <rhp1508@gmail.com>
2025-02-28 16:37:15 +0530

    PRG Pattern but not run properly

---
+++ SampleMVCWeb/Models/Feedback.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleMVCWeb.Models;
+
+public class Feedback
+{
+    public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Comment { get; set; }
+}
--- obj/Debug/net8.0/FirstApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2de77f5afe015923618d5d6b8216a1f739f1bc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c3a240cb7fdc242740d2dfd3d95df554c8533b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
