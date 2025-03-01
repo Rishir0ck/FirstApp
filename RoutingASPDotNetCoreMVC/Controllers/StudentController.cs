cceba4f20d5cb4eac037e256ab1e92bc05e5068d
Rishi Prajapati <rhp1508@gmail.com>
2025-03-01 11:04:05 +0530

    Routing Start

---
+++ RoutingASPDotNetCoreMVC/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingASPDotNetCoreMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: StudentController
+        public string Index()
+        {
+            return "Index() Action Method of StudentController";
+        }
+        public string Details(int? id)
+        {
+            return $"Details({id}) Action Method of StudentController";
+        }
+
+    }
+}
